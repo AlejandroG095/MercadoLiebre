4ee34e320aef46a42f0e572fc305e9a668410f62
Alejandro Gutierrez <alejoguti77@gmail.com>
2022-08-31 00:54:26 -0500

    app.js actualizado

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(process.env.PORT || 3030, function() {
+    console.log('Servidor corriendo en el puerto 3030');
 });
 
 app.get('/', (req,res) => {
